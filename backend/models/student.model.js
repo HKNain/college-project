6e9984b0231666c1ed539c64408040d151af649e
[ARNAV] <arnavgoyal1317@gmail.com>
2026-01-18 12:50:23 +0530

    -b/ edit studentModel.js

--- backend/models/student.model.js
+++ backend/models/student.model.js
@@ -1,36 +1,6 @@
 import { model, Schema } from "mongoose";
 
 const studentSchema = new Schema({
-  firstName: {
-    type: String,
-    required: true,
-    trim: true,
-    maxlength: 30,
-  },
-  lastName: {
-    type: String,
-    trim: true,
-    maxlength: 30,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    lowercase: true,
-    trim: true,
-    match: [
-      /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
-      "Please fill a valid email address",
-    ],
-  },
-  marks: {
-    type: Number,
-    default: 0,
-  },
-  rollNo: {
-    type: Number,
-    required: true,
-  },
   year: {
     type: Number,
     required: true,
@@ -40,10 +10,52 @@ const studentSchema = new Schema({
     required: true,
     default: "",
   },
-  isAbsent: {
-    type: Boolean,
-    default: false,
-  },
+  batch : {
+    type : String  ,
+    required : true ,
+    default : "",
+  } ,
+  data : [
+    {
+      rollNo: {
+        type: Number,
+        required: true,
+      },
+      firstName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxlength: 30,
+      },
+      lastName: {
+        type: String,
+        trim: true,
+        maxlength: 30,
+      },
+      email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+        match: [
+          /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
+          "Please fill a valid email address",
+        ],
+      },
+      marks: {
+        type: Number,
+        default: 0,
+      },
+      isAbsent: {
+        type: Boolean,
+        default: false  ,
+      },
+    }
+
+  ]
+
+  
 });
 
 const Student = model("Student", studentSchema);
